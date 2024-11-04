cb9fe161bf06776b1bd06ae7f3bfbe0e2358295e
rodrigocosta2102 <a14646@esars.pt>
2024-11-04 12:52:36 +0000

    criei a calculadora e adicionei a função para operações

---
+++ calc.py
@@ -0,0 +1,17 @@
+# Função para operações
+def soma(x, y):
+    return x + y
+
+
+def subtrair(x, y):
+    return x - y
+
+
+def multiplicar(x, y):
+    return x * y
+
+
+def dividir(x, y):
+    if y == 0:
+        return "Erro: Divisão por zero!"
+    return x / y
