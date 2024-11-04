9dede45c6b52c1c07cf666402a741bc9f766d0fe
rodrigocosta2102 <a14646@esars.pt>
2024-11-04 12:55:38 +0000

    comecei a funcao prinipal e adicionei um meno de operações

--- calc.py
+++ calc.py
@@ -15,3 +15,16 @@ def dividir(x, y):
     if y == 0:
         return "Erro: Divisão por zero!"
     return x / y
+
+# Função principal da calculadora
+def calculadora():
+    while True:
+        print("\nEscolha a operação:")
+        print("1. Soma")
+        print("2. Subtração")
+        print("3. Multiplicação")
+        print("4. Divisão")
+        print("5. Ver histórico")
+        print("6. Sair")
+    
+        escolha = input("Digite o número da operação (1/2/3/4/5/6): ")
