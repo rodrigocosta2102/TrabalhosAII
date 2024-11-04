63414548119e4b903f391efdaff8cd858b986da2
rodrigocosta2102 <a14646@esars.pt>
2024-11-04 12:58:19 +0000

    acabei a funcao principal da calculadora

--- calc.py
+++ calc.py
@@ -28,3 +28,24 @@ def calculadora():
         print("6. Sair")
     
         escolha = input("Digite o número da operação (1/2/3/4/5/6): ")
+        num1 = int(input("Digite o primeiro número: "))
+        num2 = int(input("Digite o segundo número: "))
+
+        if escolha == '1':
+            resultado = soma(num1, num2)
+            operacao = "soma"
+        elif escolha == '2':
+            resultado = subtrair(num1, num2)
+            operacao = "subtração"
+        elif escolha == '3':
+            resultado = multiplicar(num1, num2)
+            operacao = "multiplicação"
+        elif escolha == '4':
+            resultado = dividir(num1, num2)
+            operacao = "divisão"
+        elif escolha == '6':
+            print("Encerrando a calculadora...")
+            break
+        else:
+            print("Escolha inválida")
+        continue
