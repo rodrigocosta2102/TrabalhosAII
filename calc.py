ff83fdd6948b33b69119b2929c5ca46572a97aa9
rodrigocosta2102 <a14646@esars.pt>
2024-11-04 12:14:00 +0000

    adicionei um menu de opções e comecei a funcao principal da calculadora

--- calc.py
+++ calc.py
@@ -9,8 +9,17 @@ def multiplicar(x, y):
   return x * y
 
 def dividir(x, y):
-if y== 0:
+if y == 0:
         return "Erro: Divisão por zero!"
     return x / y
 
-
+# Função principal da calculadora
+def calculadora():
+    while True:
+        print("\nEscolha a operação:")
+        print("1. Soma")
+        print("2. Subtração")
+        print("3. Multiplicação")
+        print("4. Divisão")
+        print("5. Ver histórico")
+        print("6. Sair")
