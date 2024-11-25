71c119c006a06e30a34a5c37de516379e712a41c
rodrigocosta2102 <a14646@esars.pt>
2024-11-25 12:31:28 +0000

    adicionei funcoes para a loja.py
    Adicionei função para adicionar produtos, atualizar o preço de um produto, verificar a quantidade em estoque e para imprimir os produtos

--- loja.py
+++ loja.py
@@ -5,3 +5,30 @@ loja = {
     "produto3": {"nome": "Lápis Grafite 2HB", "preço": 4.80, "quantidade": 89},
     "produto4": {"nome": "Mochila Mini Troley Spiderman", "preço": 30, "quantidade": 66 }
 }
+
+#Adicionar um novo produto
+def add_produto(cod_produto, nome, preco, quantidade):
+    produto5 = {"nome": nome, "preço": preco, "quantidade": quantidade}
+    loja[cod_produto] = produto5
+
+
+
+#Imprimir os produtos
+def print_produtos():
+    for c,v in loja.items():
+        print(c,v)
+    print("---------------------------------")
+
+#Atualizar o preço de um produto
+def update_produto4(produto4, novo_preco):
+    produto4_upd= loja[produto4]
+    produto4_upd["preço"]=novo_preco
+
+
+#Verificar a quantidade em estoque
+def verificar_estoque(produto):
+    if produto in loja:
+        quantidade = loja[produto]["quantidade"]
+        return f"A quantidade em estoque de {produto} é: {quantidade}"
+    else:
+        return f"O produto '{produto}' não está disponível em estoque"
