68e512bddded420933e1421075fde2e7fb9c14dd
rodrigocosta2102 <a14646@esars.pt>
2024-11-25 12:28:01 +0000

    criei o ficheiro loja.py e adicionei uma lista de produtos

---
+++ loja.py
@@ -0,0 +1,7 @@
+#Dict para a loja
+loja = {
+    "produto1": {"nome": "Caderno Espiral A5 Pautado", "preço": 1.80, "quantidade": 54},
+    "produto2": {"nome": "Borracha Branca", "preço": 1.30, "quantidade": 157},
+    "produto3": {"nome": "Lápis Grafite 2HB", "preço": 4.80, "quantidade": 89},
+    "produto4": {"nome": "Mochila Mini Troley Spiderman", "preço": 30, "quantidade": 66 }
+}
